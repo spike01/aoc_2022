4d20f303fb2c324820167452bf3814d958fbb32f
Spike Lindsey <spike01@gmail.com>
2022-12-01 00:42:40 -0500

    day1part1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+
+fn main() -> std::io::Result<()> {
+    let lines = read_lines("./input.txt");
+
+    let mut counter = 0;
+    let mut max = 0;
+
+    for line in lines?.flatten() {
+        if line == *"".to_string() {
+            if counter > max {
+                max = counter
+            }
+            counter = 0;
+            continue;
+        }
+        counter += line.parse::<i32>().unwrap()
+    }
+    if counter > max {
+        max = counter
+    }
+    println!("Max: {}", max);
+    Ok(())
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
 }
