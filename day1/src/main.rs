4de135d75fa69fc283118f2746f8d8c4aeb58bba
Spike Lindsey <spike01@gmail.com>
2022-12-01 00:53:18 -0500

    day1part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,6 +3,10 @@ use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
 fn main() -> std::io::Result<()> {
+    part2()
+}
+
+fn part1() -> std::io::Result<()> {
     let lines = read_lines("./input.txt");
 
     let mut counter = 0;
@@ -22,6 +26,30 @@ fn main() -> std::io::Result<()> {
         max = counter
     }
     println!("Max: {}", max);
+    Ok(())
+}
+
+fn part2() -> std::io::Result<()> {
+    let lines = read_lines("./input.txt");
+
+    let mut counter = 0;
+    let mut counts = vec![];
+
+    for line in lines?.flatten() {
+        if line == *"".to_string() {
+            counts.push(counter);
+            counter = 0;
+            continue;
+        }
+        counter += line.parse::<i32>().unwrap()
+    }
+    counts.push(counter);
+
+    counts.sort_by(|a, b| b.cmp(a));
+    let top_3 = counts.into_iter().take(3).collect::<Vec<i32>>();
+    println!("Top 3: {:?}", top_3);
+    println!("Summed: {:?}", top_3.iter().sum::<i32>());
+
     Ok(())
 }
 
