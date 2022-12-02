43c2b1a8e6f9a449f3d3ffef14b5231d50df2ac6
Spike Lindsey <spike01@gmail.com>
2022-12-02 01:23:56 -0500

    Use some damn constants

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,57 +2,64 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
+const DRAW: i32 = 3;
+const WIN: i32 = 6;
+const LOSE: i32 = 0;
+
+const ROCK: i32 = 1;
+const PAPER: i32 = 2;
+const SCISSORS: i32 = 3;
+
 fn main() -> std::io::Result<()> {
     println!("===part1===");
-    part1()?;
+    println!("Total score: {}", part1()?);
     println!("===part2===");
-    part2()?;
+    println!("Total score: {}", part2()?);
     Ok(())
 }
 
-fn part1() -> std::io::Result<()> {
+fn part1() -> std::io::Result<i32> {
     let lines = read_lines("./input.txt");
+
     let mut total = 0;
+
     for line in lines?.flatten() {
-        match line.as_str() {
-            "A X" => total += 1 + 3, // println!("Rock Rock"),
-            "A Y" => total += 2 + 6, // println!("Rock Paper"),
-            "A Z" => total += 3 + 0, // println!("Rock Scissors"),
-            "B X" => total += 1 + 0, // println!("Paper Rock"),
-            "B Y" => total += 2 + 3, // println!("Paper Paper"),
-            "B Z" => total += 3 + 6, // println!("Paper Scissors"),
-            "C X" => total += 1 + 6, // println!("Scissors Rock"),
-            "C Y" => total += 2 + 0, // println!("Scissors Paper"),
-            "C Z" => total += 3 + 3, // println!("Scissors Scissors"),
-            _ => println!("Invalid move!"),
+        total += match line.as_str() {
+            "A X" => DRAW + ROCK,
+            "A Y" => WIN + PAPER,
+            "A Z" => LOSE + SCISSORS,
+            "B X" => LOSE + ROCK,
+            "B Y" => DRAW + PAPER,
+            "B Z" => WIN + SCISSORS,
+            "C X" => WIN + ROCK,
+            "C Y" => LOSE + PAPER,
+            "C Z" => DRAW + SCISSORS,
+            _ => unreachable!(),
         }
     }
-    println!("Total score: {}", total);
-
-    Ok(())
+    Ok(total)
 }
 
-fn part2() -> std::io::Result<()> {
+fn part2() -> std::io::Result<i32> {
     let lines = read_lines("./input.txt");
 
     let mut total = 0;
+
     for line in lines?.flatten() {
-        match line.as_str() {
-            "A X" => total += 0 + 3, // println!("Lose with Scissors"),
-            "A Y" => total += 3 + 1, // println!("Draw with Rock"),
-            "A Z" => total += 6 + 2, // println!("Win with Paper"),
-            "B X" => total += 0 + 1, // println!("Lose with Rock"),
-            "B Y" => total += 3 + 2, // println!("Draw with Paper"),
-            "B Z" => total += 6 + 3, // println!("Win with Scissors"),
-            "C X" => total += 0 + 2, // println!("Lose with Paper"),
-            "C Y" => total += 3 + 3, // println!("Draw with Scissors"),
-            "C Z" => total += 6 + 1, // println!("Win with Rock"),
-            _ => println!("Invalid move!"),
+        total += match line.as_str() {
+            "A X" => LOSE + SCISSORS,
+            "A Y" => DRAW + ROCK,
+            "A Z" => WIN + PAPER,
+            "B X" => LOSE + ROCK,
+            "B Y" => DRAW + PAPER,
+            "B Z" => WIN + SCISSORS,
+            "C X" => LOSE + PAPER,
+            "C Y" => DRAW + SCISSORS,
+            "C Z" => WIN + ROCK,
+            _ => unreachable!(),
         }
     }
-    println!("Total score: {}", total);
-
-    Ok(())
+    Ok(total)
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
