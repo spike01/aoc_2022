abec1e644c6022c9208fd7a02d779dd4f360bb36
Spike Lindsey <spike01@gmail.com>
2022-12-07 22:46:50 -0500

    day7part1+2 in rust

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,126 +1,77 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
-use std::collections::HashMap;
 
 fn main() -> std::io::Result<()> {
     println!("===part1===");
-    part1()?;
+    println!("{}", part1()?);
     println!("===part2===");
-    part2()?;
+    println!("{}", part2()?);
     Ok(())
 }
 
-#[derive(Debug)]
-struct State {
-    pwd: String,
-    filetree: HashMap<String, AocFile>,
-    is_reading_output: bool,
-    output: Vec<String>
+fn part1() -> std::io::Result<i32> {
+    let lines = read_lines("./input.txt")?;
+    let files = directory_sizes(lines);
+
+    let sum_at_most_100_000 = files.iter().filter(|&s| *s <= 100_000).sum();
+
+    Ok(sum_at_most_100_000)
 }
 
-impl State {
-    fn print_filetree(&self) {
-        println!("{:?}", self.filetree.get("/").unwrap().subdirs.as_ref().unwrap());
-    }
+fn part2() -> std::io::Result<i32> {
+    let lines = read_lines("./input.txt")?;
+    let files = directory_sizes(lines);
 
-    fn change_directory(&mut self, dest: &str) {
-        self.pwd = dest.to_string();
-    }
+    let total_space = 70_000_000;
+    let space_needed = 30_000_000;
+    let available_space = total_space - files[0]; // root
 
-    fn process_output(&mut self) {
-        for line in self.output.iter() {
-            if let Some((info, name)) = line.split_once(" ") {
-                if info == "dir" {
-                    let file = AocFile {
-                        name: name.to_string(),
-                        size: 0,
-                        is_directory: true,
-                        subdirs: Some(HashMap::new())
-                    };
-                    println!("{:?}", file);
+    let deletion_candidate = files
+        .iter()
+        .filter(|&s| (available_space + *s) >= space_needed)
+        .min()
+        .expect("Expect an answer to exist");
 
-                    let root = self.filetree.get("/").unwrap();
-                    let mut subdirs = root.subdirs.unwrap();
-                    subdirs.insert(name.to_string(), file);
-                    //subdirs.entry(name.to_string()).or_insert(file);
+    Ok(*deletion_candidate)
+}
 
-                } else {
-                    //let file = AocFile {
-                        //name: name.to_string(),
-                        //size: info.parse().unwrap(),
-                        //is_directory: false,
-                        //subdirs: None
-                    //};
-                    //println!("{:?}", file);
-                    //if let Entry::Occupied(e) = self.filetree.entry("/".to_string()) {
-                       //let subdirs: &mut HashMap<String, AocFile> = &mut e.get().subdirs.unwrap();
-                       //subdirs.insert(name.to_string(), file);
-                    //}
-                    //println!("{:?}", self.filetree.entry("/".to_string()));
+fn directory_sizes(lines: std::io::Lines<BufReader<File>>) -> Vec<i32> {
+    let mut size_stack: Vec<i32> = Vec::new();
+    let mut file_sizes: Vec<i32> = Vec::new();
+
+    for line in lines.flatten() {
+        if line.starts_with('$') {
+            let command = &line[2..4];
+            match command {
+                "cd" => {
+                    let dest = &line[5..];
+                    if dest == ".." {
+                        if let Some(size) = size_stack.pop() {
+                            file_sizes.push(size);
+                        }
+                        continue;
+                    }
+                    size_stack.push(0);
+                }
+                "ls" => continue,
+                _ => unreachable!(),
+            }
+            continue;
+        }
+
+        if let Some((info, _)) = line.split_once(' ') {
+            if info != "dir" {
+                for f in size_stack.iter_mut() {
+                    *f += info.parse::<i32>().expect("should fit!");
                 }
             }
         }
-         self.print_filetree();
-         self.is_reading_output = false;
-         self.output.clear();
-}
-}
-
-#[derive(Debug)]
-struct AocFile {
-    name: String,
-    size: i32,
-    is_directory: bool,
-    subdirs: Option<HashMap<String, AocFile>>
-}
-
-fn part1() -> std::io::Result<()> {
-    let lines = read_lines("./input_smol.txt");
-
-    let mut file = AocFile {
-        name: "/".to_string(),
-        size: 0,
-        is_directory: true,
-        subdirs: Some(HashMap::new())
-    };
-
-    let mut state = State {
-        pwd: "/".to_string(),
-        filetree: HashMap::new(),
-        is_reading_output: false,
-        output: Vec::new()
-    };
-
-    state.filetree.insert("/".to_string(), file);
-
-    for line in lines?.flatten() {
-        if line.starts_with("$ cd") {
-            if state.is_reading_output {
-                state.process_output();
-            }
-            let (_, dest) = line.split_at(5);
-            state.change_directory(dest);
-            println!("pwd: {}", state.pwd);
-        }
-        if line.starts_with("$ ls") {
-            state.is_reading_output = true;
-            continue
-        }
-        if state.is_reading_output && !line.starts_with("$") {
-            state.output.push(line.to_string());
-            continue
-        }
     }
+    // append this way around because size_stack[0] is the root file_size
+    size_stack.append(&mut file_sizes);
 
-    Ok(())
-}
-
-
-fn part2() -> std::io::Result<()> {
-    let _lines = read_lines("./input_smol.txt");
-
-    Ok(())
+    size_stack
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
