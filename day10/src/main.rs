848ddbf8a1347a8bc29dfee99e763712dafee951
Spike Lindsey <spike01@gmail.com>
2022-12-13 10:15:47 -0500

    day10 tidying

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -3,11 +3,14 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
+const ROW_LENGTH: usize = 40;
+const MAX_COUNTER: usize = 240;
+
 fn main() -> std::io::Result<()> {
     println!("===part1===");
     println!("{}", part1()?);
     println!("===part2===");
-    part2()?;
+    part2()?.iter().for_each(|line| println!("{line}"));
     Ok(())
 }
 
@@ -15,31 +18,20 @@ fn part1() -> std::io::Result<i32> {
     let lines = read_lines("./input.txt")?;
 
     let mut register = 1;
-    let mut counter = 1;
+    let mut counter = 0;
     let mut signal_strength = 0;
 
     let checkpoints = vec![20, 60, 100, 140, 180, 220];
-    let mut cycles: VecDeque<i32> = VecDeque::new();
+    let mut cycles = cycles(lines);
 
-    for line in lines.flatten() {
-        match line.split_once(' ') {
-            Some(("addx", x)) => {
-                cycles.push_back(0);
-                cycles.push_back(x.parse::<i32>().unwrap());
-            }
-            None => cycles.push_back(0),
-            Some((_, _)) => unreachable!(),
-        }
-    }
-
-    cycles.pop_front();
-
-    while counter <= 240 {
-        counter += 1;
+    while counter < MAX_COUNTER {
+        counter += 1; // registers are checked at end of cycle
 
         if checkpoints.contains(&counter) {
-            println!("{counter} * {register} = {}", register * counter as i32);
-            signal_strength += register * counter as i32;
+            let strength = register * counter as i32;
+            signal_strength += strength;
+
+            println!("{counter} * {register} = {strength}");
         }
 
         if let Some(tick) = cycles.pop_front() {
@@ -50,13 +42,41 @@ fn part1() -> std::io::Result<i32> {
     Ok(signal_strength)
 }
 
-fn part2() -> std::io::Result<()> {
+fn part2() -> std::io::Result<Vec<String>> {
     let lines = read_lines("./input.txt")?;
 
-    let mut register: i32 = 1;
-    let mut counter: usize = 0;
+    let mut register = 1;
+    let mut counter = 0;
 
     let mut output: Vec<char> = Vec::new();
+    let mut cycles = cycles(lines);
+
+    while counter < MAX_COUNTER {
+        let line_adjusted_counter = (counter % ROW_LENGTH) as i32;
+
+        match line_adjusted_counter {
+            _ if register == line_adjusted_counter => output.push('#'),
+            _ if register + 1 == line_adjusted_counter => output.push('#'),
+            _ if register - 1 == line_adjusted_counter => output.push('#'),
+            _ => output.push('.'),
+        }
+
+        counter += 1;
+
+        if let Some(tick) = cycles.pop_front() {
+            register += tick;
+        }
+    }
+
+    let printable_output = output
+        .chunks(ROW_LENGTH)
+        .map(|chunk| chunk.iter().collect::<String>())
+        .collect();
+
+    Ok(printable_output)
+}
+
+fn cycles(lines: std::io::Lines<BufReader<File>>) -> VecDeque<i32> {
     let mut cycles: VecDeque<i32> = VecDeque::new();
 
     for line in lines.flatten() {
@@ -70,36 +90,7 @@ fn part2() -> std::io::Result<()> {
         }
     }
 
-
-    while counter <= 240 {
-        let sprite_position = vec![register - 1, register, register + 1];
-
-        match counter {
-            _ if sprite_position.contains(&(counter as i32 % 40)) => output.push('#'),
-            _ => output.push('.')
-        }
-
-        counter += 1;
-
-        if let Some(tick) = cycles.pop_front() {
-            register += tick;
-        }
-    }
-
-    let line_1: String = output[0..39].iter().collect();
-    let line_2: String = output[40..79].iter().collect();
-    let line_3: String = output[80..119].iter().collect();
-    let line_4: String = output[120..159].iter().collect();
-    let line_5: String = output[160..199].iter().collect();
-    let line_6: String = output[200..239].iter().collect();
-
-    println!("{}", line_1);
-    println!("{}", line_2);
-    println!("{}", line_3);
-    println!("{}", line_4);
-    println!("{}", line_5);
-    println!("{}", line_6);
-    Ok(())
+    cycles
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
