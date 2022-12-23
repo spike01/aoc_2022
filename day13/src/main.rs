32c4315f15ea2639aab2e07ab788beae9213942a
Spike Lindsey <spike01@gmail.com>
2022-12-23 15:37:01 -0500

    day13 introduce nested lists

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,96 +1,37 @@
+use crate::List::{Cons, Nil};
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
-use crate::List::{Cons, Nil};
 
 fn main() -> std::io::Result<()> {
     println!("===part1===");
-    part1()?;
+    println!("{}", part1()?);
     println!("===part2===");
     part2()?;
     Ok(())
 }
 
-#[derive(Debug, Clone)]
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
-
-impl List {
-    fn empty() -> List {
-        Nil
-    }
-
-    fn cons(val: i32, list: List) -> List {
-       Cons(val, Box::new(list))
-    }
-
-    // not working yet
-    fn append(list1: List, list2: List) -> List {
-       if Self::is_empty(&list1) {
-            return list2;
-       }
-       // (cons
-       //   (car ls1)
-       //   (append (cdr ls1) ls2)))))
-       Self::cons(
-           Self::head(&list1).unwrap(),
-           Self::append(Self::tail(&list1).unwrap(), list2)
-       )
-    }
-
-    fn is_empty(list: &List) -> bool {
-       match list {
-          Nil => true,
-          _ => false
-       }
-    }
-
-    // car
-    fn head(list: &List) -> Option<i32> {
-       match list {
-         Cons(i, _) => Some(*i),
-         Nil => None
-       }
-    }
-
-    // cdr
-    fn tail(list: &List) -> Option<List> {
-        match list {
-            Cons(_, list) => Some(*list.clone()),
-            Nil => None
-        }
-    }
-
-    fn last(list: List) -> Option<i32> {
-        let mut current = list;
-        while let Some(cons) = Self::tail(&current) {
-            current = cons;
-        }
-        Self::head(&current)
-    }
-}
-
-fn part1() -> std::io::Result<()> {
+fn part1() -> std::io::Result<usize> {
     let lines = read_lines("./input_smol.txt")?;
 
     let mut list1 = List::empty();
     let mut list2 = List::empty();
-    let mut left = true;
+
+    let mut is_left = true;
+    let mut count = 0;
+    let mut idx = 0;
 
     for line in lines.flatten() {
-
         if line.is_empty() {
-            left = true;
+            is_left = true;
             list1 = List::empty();
             list2 = List::empty();
             continue;
         }
 
-        if left {
+        if is_left {
             list1 = list_from(&line);
-            left = false;
+            is_left = false;
             continue;
         }
 
@@ -98,16 +39,92 @@ fn part1() -> std::io::Result<()> {
 
         println!("{list1:?}");
         println!("{list2:?}");
+
+        idx += 1;
+
+        if List::are_ordered(&list1, &list2) {
+            count += idx;
+        }
+    }
+
+    Ok(count)
+}
+
+fn part2() -> std::io::Result<()> {
+    let lines = read_lines("./input_smol.txt")?;
+
+    for line in lines.flatten() {
+        println!("{line}");
     }
 
     Ok(())
+}
+
+#[derive(Debug, Clone)]
+enum List {
+    Cons(Value, Box<List>),
+    Nil,
+}
+
+#[derive(Debug, Clone)]
+enum Value {
+    Int(i32),
+    NestedList(Box<List>),
+}
+
+impl List {
+    fn empty() -> List {
+        Nil
+    }
+
+    fn cons(val: Value, list: List) -> List {
+        Cons(val, Box::new(list))
+    }
+
+    fn append(list1: List, list2: List) -> List {
+        if Self::is_empty(&list1) {
+            return list2;
+        }
+        // (cons
+        //   (car ls1)
+        //   (append (cdr ls1) ls2)))))
+        Self::cons(
+            Self::head(&list1).expect("List already checked as non-empty"),
+            Self::append(Self::tail(&list1).unwrap(), list2),
+        )
+    }
+
+    fn is_empty(list: &List) -> bool {
+        matches!(list, Nil)
+    }
+
+    // car/first
+    fn head(list: &List) -> Option<Value> {
+        match list {
+            Cons(Value::Int(i), _) => Some(Value::Int(*i)),
+            Cons(Value::NestedList(l), _) => Some(Value::NestedList(Box::new(*l.clone()))),
+            _ => None,
+        }
+    }
+
+    // cdr/rest
+    fn tail(list: &List) -> Option<List> {
+        match list {
+            Cons(_, list) => Some(*list.clone()),
+            Nil => None,
+        }
+    }
+
+    fn are_ordered(list1: &List, list2: &List) -> bool {
+        true
+    }
 }
 
 #[derive(Debug)]
 enum Token {
     Open,
     Close,
-    Value(i32)
+    Value(i32),
 }
 
 fn list_from(line: &str) -> List {
@@ -124,7 +141,7 @@ fn list_from(line: &str) -> List {
                     current_value.clear();
                 }
                 tokens.push(Token::Close)
-            },
+            }
             ' ' => continue,
             ',' => {
                 if !current_value.is_empty() {
@@ -132,8 +149,8 @@ fn list_from(line: &str) -> List {
                     tokens.push(Token::Value(s.parse::<i32>().unwrap()));
                     current_value.clear();
                 }
-            },
-            x => current_value.push(x)
+            }
+            x => current_value.push(x),
         }
     }
 
@@ -143,21 +160,11 @@ fn list_from(line: &str) -> List {
         match token {
             Token::Open => (),
             Token::Close => (),
-            Token::Value(i) => list = List::append(list, List::cons(*i, List::empty())),
+            Token::Value(i) => list = List::append(list, List::cons(Value::Int(*i), List::empty())),
         }
     }
 
     list
-}
-
-fn part2() -> std::io::Result<()> {
-    let lines = read_lines("./input_smol.txt")?;
-
-    for line in lines.flatten() {
-        println!("{line}");
-    }
-
-    Ok(())
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
